8ab0055dffe3c040639568a6ba0a65c45d75eb47
isheebo <kedgar751@gmail.com>
2017-10-16 11:49:35 +0300

    add the create_app helper function

---
+++ app/__init__.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+from config import app_config
+
+db = SQLAlchemy()
+
+
+def create_app(configuration="development"):
+    app = Flask(__name__)
+    app.config.from_object(app_config[configuration])
+    db.init_app(app)
+
+    # register blueprints
+    return app
