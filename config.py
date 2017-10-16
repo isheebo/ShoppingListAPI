d1a267fd1b4067c9a2687bf2e7b991283560146d
isheebo <kedgar751@gmail.com>
2017-10-16 12:10:37 +0300

    add tests for the app's configuration

--- config.py
+++ config.py
@@ -14,18 +14,18 @@ class TestingConfig(BaseConfig):
     TESTING = True
     DEBUG = True
     AUTH_EXPIRY_TIME_IN_SECONDS = 3  # just 3 seconds and the token expires
-    SQLALCHEMY_DATABASE_URL = os.getenv(
+    SQLALCHEMY_DATABASE_URI = os.getenv(
         'DATABASE_URL', "postgresql://postgres:pumpkin@localhost:5432/ShoppingListTest")
 
 
 class DevelopmentConfig(BaseConfig):
-    SQLALCHEMY_DATABASE_URL = os.getenv(
+    SQLALCHEMY_DATABASE_URI = os.getenv(
         'DATABASE_URL', "postgresql://postgres:pumpkin@localhost:5432/ShoppingList")
     DEBUG = True
 
 
 class ProductionConfig(BaseConfig):
-    SQLALCHEMY_DATABASE_URL = os.getenv('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
 
 
 app_config = dict(testing=TestingConfig,
---
+++ tests/test_config.py
@@ -0,0 +1,48 @@
+import unittest
+import os
+from app import create_app
+
+
+class TestTestingConfiguration(unittest.TestCase):
+    def setUp(self):
+        self.test_app = create_app("testing")
+
+    def test_app_is_created_with_right_configuration(self):
+        self.assertTrue(self.test_app.config['DEBUG'])
+        self.assertTrue(self.test_app.config['TESTING'])
+        self.assertEqual(
+            self.test_app.config['AUTH_EXPIRY_TIME_IN_SECONDS'], 3)
+        self.assertEqual(self.test_app.config['SQLALCHEMY_DATABASE_URI'],
+                         "postgresql://postgres:pumpkin@localhost:5432/ShoppingListTest")
+        self.assertFalse(
+            self.test_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'])
+
+
+class TestDevelopmentConfiguration(unittest.TestCase):
+    def setUp(self):
+        self.test_app = create_app()
+
+    def test_app_is_created_with_development_configuration(self):
+        self.assertFalse(self.test_app.config['TESTING'])
+        self.assertTrue(self.test_app.config['DEBUG'])
+        self.assertEqual(self.test_app.config['SQLALCHEMY_DATABASE_URI'],
+                         "postgresql://postgres:pumpkin@localhost:5432/ShoppingList")
+        self.assertFalse(
+            self.test_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'])
+        self.assertEqual(
+            self.test_app.config['AUTH_EXPIRY_TIME_IN_SECONDS'], 3600)
+
+
+class TestProductionConfiguration(unittest.TestCase):
+    def setUp(self):
+        self.test_app = create_app("production")
+
+    def test_app_is_created_with_production_configuration(self):
+        self.assertFalse(self.test_app.config['DEBUG'])
+        self.assertFalse(self.test_app.config['TESTING'])
+        self.assertFalse(
+            self.test_app.config['SQLALCHEMY_TRACK_MODIFICATIONS'])
+        self.assertEqual(
+            self.test_app.config['AUTH_EXPIRY_TIME_IN_SECONDS'], 3600)
+        self.assertTrue(
+            self.test_app.config['SQLALCHEMY_DATABASE_URI'], os.getenv('DATABASE_URL'))
