6afaace6ab5047d603c5ac2d6ce9e1a6c348df06
isheebo <kedgar751@gmail.com>
2017-10-21 11:36:02 +0300

    create a Base class for all tests

--- tests/__init__.py
+++ tests/__init__.py
@@ -4,15 +4,14 @@ from app import create_app, db
 
 class BaseTests(unittest.TestCase):
     def setUp(self):
-        self.test_app = create_app("testing")
-        self.app_context = self.test_app.app_context()
+        self.app = create_app("testing")
+        self.test_client = self.app.test_client()
+        self.app_context = self.app.app_context()
         self.app_context.push()
         db.drop_all()
         db.create_all()
 
-        self.test_client = self.test_app.test_client()
-
     def tearDown(self):
         db.session.remove()
         db.drop_all()
-        self.test_app.app_context().pop()
+        self.app_context.pop()
