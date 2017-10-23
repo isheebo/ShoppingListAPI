ca7f7a9ea0eddc353fff12dedc1011b2673237fc
isheebo <kedgar751@gmail.com>
2017-10-23 19:01:00 +0300

    add the put_item_by_id endpoint

--- app/endpoints/items/views.py
+++ app/endpoints/items/views.py
@@ -3,7 +3,7 @@ from flask.views import MethodView
 from flask import Blueprint, request, jsonify
 
 from app.models import Item
-from app.endpoints import parse_auth_header, get_shoppinglist
+from app.endpoints import parse_auth_header, get_shoppinglist, get_item
 
 items = Blueprint("items", __name__, url_prefix="/api/v1")
 
@@ -65,6 +65,7 @@ class ItemsAPI(MethodView):
                 "status": status,
                 "message": message
             }), status_code
+
         shoppinglist, message, status, status_code = get_shoppinglist(
             user_id, list_id)
         if shoppinglist:
@@ -92,10 +93,14 @@ class ItemsAPI(MethodView):
                 list_items.append({
                     "id": item.id,
                     "name": item.name,
+                    "price": item.price,
+                    "quantity": item.quantity,
+                    "has been bought": item.has_been_bought,
                     "date modified": item.date_modified.strftime("%Y-%m-%d %H:%M:%S"),
                     "current page": page,
                     "total number of pages": pagination_object.pages
                 })
+
             if list_items:
                 if q is not None:
                     return jsonify({
@@ -121,7 +126,129 @@ class ItemsAPI(MethodView):
         }), status_code
 
 
+class ItemsAPIByID(MethodView):
+    @staticmethod
+    def get(list_id, item_id):
+        user_id, message, status, status_code, _ = parse_auth_header(request)
+        if user_id is None:
+            return jsonify({
+                "status": status,
+                "message": message
+            }), status_code
+
+        item, message, status, status_code = get_item(
+            user_id, list_id, item_id)
+        if item is not None:
+            return jsonify({
+                "id": item.id,
+                "name": item.name,
+                "price": item.price,
+                "quantity": item.quantity,
+                "has been bought": item.has_been_bought,
+                "date modified": item.date_modified.strftime("%Y-%m-%d %H:%M:%S")
+            }), 200
+        return jsonify({
+            "status": status,
+            "message": message
+        }), status_code
+
+    @staticmethod
+    def delete(list_id, item_id):
+        user_id, message, status, status_code, _ = parse_auth_header(request)
+        if user_id is None:
+            return jsonify({
+                "status": status,
+                "message": message
+            }), status_code
+
+        item, message, status, status_code = get_item(
+            user_id, list_id, item_id)
+        if item is not None and item.delete():
+            return jsonify({
+                'status': status,
+                'message': f'an item with ID {item_id} has been successfully deleted'
+            }), status_code
+        return jsonify({
+            "status": status,
+            "message": message
+        }), status_code
+
+    @staticmethod
+    def put(list_id, item_id):
+        user_id, message, status, status_code, _ = parse_auth_header(request)
+        if user_id is None:
+            return jsonify({
+                "status": status,
+                "message": message
+            }), status_code
+
+        item, message, status, status_code = get_item(
+            user_id, list_id, item_id)
+        if item is not None:
+            # query parameters
+            name = request.form.get("name")
+            price = request.form.get("price")
+            quantity = request.form.get("quantity")
+            status = request.form.get("status")
+
+            has_been_bought = True if status and status.title() == "True" else False
+            if name and price and quantity:
+                name = name.strip().lower()
+                name_already_exists = Item.query.filter(Item.shoppinglist_id == list_id).filter((
+                    (Item.name == name) & (Item.id != item_id))).all()
+
+                if (item.name == name and item.quantity == quantity and  # if no edits were made...
+                        item.price == price and item.has_been_bought == has_been_bought):
+                    return jsonify({
+                        "status": "failure",
+                        "message": "no changes were made to the item"
+                    }), 200
+
+                # if i change the quantity from 3 to 2, it should save the item as
+                #  changed regardless of the name... that is
+                # if either (item.name != name) or (item.quantity != quantity) or
+                #  (item.has_been_bought != has_been_bought) or (item.price != price),
+                # then we should register the item as having been edited!
+
+                if name_already_exists:  # if name exists and it isn't the current name we are editing
+                    return jsonify({
+                        "status": 'failure',
+                        "message": f"an item with name '{name}' already exists"
+                    }), 409
+
+                item.name = name
+                item.price = price
+                item.quantity = quantity
+                item.has_been_bought = has_been_bought
+                item.date_modified = datetime.now()
+                item.save()
+                return jsonify({
+                    'status': 'success',
+                    'data': {
+                        'id': item.id,
+                        'name': item.name,
+                        "price": item.price,
+                        "quantity": item.quantity,
+                        'date modified': item.date_modified,
+                        'has been bought': item.has_been_bought
+                    },
+                    'message': 'item has been updated successfully'
+                }), 200
+            return jsonify({
+                'status': 'failure',
+                'message':  "'name', 'price' and 'quantity' of an item must be specified whereas 'status' is optional"
+            }), 400
+        return jsonify({
+            "status": status,
+            "message": message
+        }), status_code
+
+
 items_api = ItemsAPI.as_view("items_api")
+items_by_id_api = ItemsAPIByID.as_view("items_by_id_api")
 
 items.add_url_rule("/shoppinglists/<list_id>/items",
                    view_func=items_api, methods=['POST', 'GET'])
+
+items.add_url_rule("/shoppinglists/<list_id>/items/<item_id>",
+                   view_func=items_by_id_api, methods=['GET', 'DELETE', 'PUT'])
