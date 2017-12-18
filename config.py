885a85544d4d810862b7828168520df332cdc676
isheebo <kedgar751@gmail.com>
2017-12-18 12:12:30 +0300

    Modify .coveragerc

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ try:
     import settings
     TEST_DATABASE_URL = settings.TEST_DATABASE_URL
     DATABASE_URL = settings.DATABASE_URL
-except ImportError:
+except ImportError:  # pragma: no cover
     TEST_DATABASE_URL = os.getenv('TEST_DATABASE_URL')
     DATABASE_URL = os.getenv('DATABASE_URL')
 
